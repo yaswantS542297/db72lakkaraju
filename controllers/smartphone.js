2217e09daac23f4c66c01d6176aa1941cd9b78d0
yaswantS542297 <77638984+yaswantS542297@users.noreply.github.com>
2021-04-08 21:14:05 -0500

    postman

--- controllers/smartphone.js
+++ controllers/smartphone.js
@@ -19,9 +19,31 @@ exports.smartphone_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: smartphone detail: ' + req.params.id);
 };
 // Handle Smartphone create on POST.
-exports.smartphone_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: smartphone create POST');
-};
+//exports.smartphone_create_post = function(req, res) {
+// res.send('NOT IMPLEMENTED: smartphone create POST');
+//};
+
+
+exports.smartphone_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new smartphone();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.name = req.body.name;
+    document.cost = req.body.cost;
+    document.brand = req.body.brand;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
+
+
 // Handle Smartphone delete form on DELETE.
 exports.smartphone_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: smartphone delete DELETE ' + req.params.id);
@@ -35,7 +57,7 @@ exports.smartphone_update_put = function(req, res) {
 // Handle a show all view
 exports.smartphone_view_all_Page = async function(req, res) {
     try{
-    smartphones = await smartphone.find();
+    smartphone = await smartphone.find();
     res.render('smartphone', { title: 'smartphone Search Results', results: smartphones });
     }
     catch(err){
