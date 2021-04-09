23b2f030b98a016ed2f960631a9a351687083cdb
yaswantS542297 <77638984+yaswantS542297@users.noreply.github.com>
2021-04-08 20:17:41 -0500

    to diplay all

--- controllers/smartphone.js
+++ controllers/smartphone.js
@@ -1,8 +1,19 @@
 var smartphone = require('../models/smartphone');
 // List of all Smartphone
-exports.smartphone_list = function(req, res) {
- res.send('NOT IMPLEMENTED: smartphone list');
-};
+//exports.smartphone_list = function(req, res) {
+// res.send('NOT IMPLEMENTED: smartphone list');
+//};
+
+// List of all Costumes
+exports.smartphone_list = async function(req, res) {
+    try{
+    thesmartphones = await smartphone.find();
+    res.send(thesmartphones);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
 // for a specific Smartphone.
 exports.smartphone_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: smartphone detail: ' + req.params.id);
@@ -19,3 +30,15 @@ exports.smartphone_delete = function(req, res) {
 exports.smartphone_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: smartphone update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.smartphone_view_all_Page = async function(req, res) {
+    try{
+    smartphones = await smartphone.find();
+    res.render('smartphone', { title: 'smartphone Search Results', results: smartphones });
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
--- routes/smartphone.js
+++ routes/smartphone.js
@@ -1,10 +1,10 @@
 var express = require('express');
+const smartphone_controlers= require('../controllers/smartphone');
+
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('smartphone', { title: 'Search Results smartphone' });
-});
-
+/* GET smartphones */
+router.get('/', smartphone_controlers.smartphone_view_all_Page);
 module.exports = router;
 
